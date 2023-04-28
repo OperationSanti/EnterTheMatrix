360f2e8328b8272ef6291624cd8379cb518c5663
OperationSanti <128191513+OperationSanti@users.noreply.github.com>
2023-04-27 21:36:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,55 +2,55 @@ function calculateTip() {
     let billAmount = parseFloat(document.getElementById("billAmount").value);
     let tipPercentage = parseInt(document.getElementById("tipPercentage").value);
     let numPeople = parseInt(document.getElementById("numPeople").value);
-  
+
     if (isNaN(billAmount) || isNaN(tipPercentage) || isNaN(numPeople) || numPeople <= 0) {
-      document.getElementById("tipAmount").innerHTML = "Please enter valid numbers.";
-      document.getElementById("totalAmount").innerHTML = "";
-      document.getElementById("splitAmount").innerHTML = "";
-      return;
+        document.getElementById("tipAmount").innerHTML = "Please enter valid numbers.";
+        document.getElementById("totalAmount").innerHTML = "";
+        document.getElementById("splitAmount").innerHTML = "";
+        return;
     }
-  
+
     let tipAmount = 0;
     switch (tipPercentage) {
-      case 0:
-        tipAmount = billAmount * 0; // 0% tip
-        break;
-      case 1:
-        tipAmount = billAmount * 0.05; // 5% tip
-        break;
-      case 2:
-        tipAmount = billAmount * 0.25; // 25% tip
-        break;
+        case 0:
+            tipAmount = billAmount * 0; // 0% tip
+            break;
+        case 1:
+            tipAmount = billAmount * 0.05; // 5% tip
+            break;
+        case 2:
+            tipAmount = billAmount * 0.25; // 25% tip
+            break;
     }
-  
+
     let totalAmount = billAmount + tipAmount;
     let splitAmount = totalAmount / numPeople;
-  
+
     document.getElementById("tipAmount").innerHTML = "Tip Amount: $" + tipAmount.toFixed(2);
     document.getElementById("totalAmount").innerHTML = "Total Amount: $" + totalAmount.toFixed(2);
     document.getElementById("splitAmount").innerHTML = "Amount per Person: $" + splitAmount.toFixed(2);
-  }
-  
-  function updateEmoticon() {
+}
+
+function updateEmoticon() {
     let tipPercentage = parseInt(document.getElementById("tipPercentage").value);
     let emoticonText = "";
-  
+
     switch (tipPercentage) {
-      case 0:
-        emoticonText = "Sad (0%)";
-        break;
-      case 1:
-        emoticonText = "Ok (5%)";
-        break;
-      case 2:
-        emoticonText = "Happy (25%)";
-        break;
+        case 0:
+            emoticonText = "Sad (0%)";
+            break;
+        case 1:
+            emoticonText = "Ok (5%)";
+            break;
+        case 2:
+            emoticonText = "Happy (25%)";
+            break;
     }
-  
+
     document.getElementById("emoticon").innerText = emoticonText;
-  }
-  
-  function handleSignatureInput() {
+}
+
+function handleSignatureInput() {
     const signatureInput = document.getElementById("signature");
     const enteredSignature = signatureInput.value;
 
@@ -85,5 +85,4 @@ function resetCalculator() {
     document.getElementById("splitAmount").innerHTML = "";
     document.getElementById("signature").value = "";
     document.getElementById("signature").style.display = "block";
-  }
-  
+}
